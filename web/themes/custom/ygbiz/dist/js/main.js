a664c14651ce86849e75fce71ace05550b3e8895
Bryan Aldrin Quinalayo <bryan.aldrin.quinalayo@codeandtheory.com>
2020-02-27 19:17:58 +0800

    DPL-732: Projects Component All

--- web/themes/custom/ygbiz/dist/js/main.js
+++ web/themes/custom/ygbiz/dist/js/main.js
@@ -1,55 +1,58 @@
 jQuery(document).ready(function($) {
-  $('.blog-carousel').owlCarousel({
+  $(".blog-carousel").owlCarousel({
     autoplay: true,
     dots: false,
     loop: true,
     nav: true,
     responsive: { 0: { items: 1 }, 768: { items: 1 }, 900: { items: 2 } }
   });
-  $('.owl-prev').html('<i class="fa fa-chevron-left"></i>');
-  $('.owl-next').html('<i class="fa fa-chevron-right"></i>');
-  $('.portfolio-popup').magnificPopup({
-    type: 'image',
+  $(".owl-prev").html('<i class="fa fa-chevron-left"></i>');
+  $(".owl-next").html('<i class="fa fa-chevron-right"></i>');
+  $(".portfolio-popup").magnificPopup({
+    type: "image",
     removalDelay: 300,
-    mainClass: 'mfp-fade',
+    mainClass: "mfp-fade",
     gallery: { enabled: true },
     zoom: {
       enabled: true,
       duration: 300,
-      easing: 'ease-in-out',
+      easing: "ease-in-out",
       opener: function(openerElement) {
-        return openerElement.is('img')
+        return openerElement.is("img")
           ? openerElement
-          : openerElement.find('img');
+          : openerElement.find("img");
       }
     }
   });
   new WOW().init();
-  $('.loop').owlCarousel({
+  $(".loop").owlCarousel({
     loop: true,
     margin: 10,
     responsive: { 0: { items: 1 }, 600: { items: 2 }, 1000: { items: 3 } }
   });
-  $('.testimonial').owlCarousel({
+  $(".testimonial").owlCarousel({
     loop: true,
     margin: 10,
     responsive: { 0: { items: 1 }, 600: { items: 1 }, 1000: { items: 1 } }
   });
 });
-$(window).on('load', function() {
-  var $container = $('.portfolioContainer');
-  $container.isotope({
-    filter: '*',
-    animationOptions: { duration: 750, easing: 'linear', queue: false }
-  });
-  $('.portfolioFilter a').click(function() {
-    $('.portfolioFilter .current').removeClass('current');
-    $(this).addClass('current');
-    var selector = $(this).attr('data-filter');
+$(window).on("load", function() {
+  var $container = $(".portfolioContainer");
+
+  if ($container) {
     $container.isotope({
-      filter: selector,
-      animationOptions: { duration: 750, easing: 'linear', queue: false }
+      filter: "*",
+      animationOptions: { duration: 750, easing: "linear", queue: false }
     });
-    return false;
-  });
+    $(".portfolioFilter a").click(function() {
+      $(".portfolioFilter .current").removeClass("current");
+      $(this).addClass("current");
+      var selector = $(this).attr("data-filter");
+      $container.isotope({
+        filter: selector,
+        animationOptions: { duration: 750, easing: "linear", queue: false }
+      });
+      return false;
+    });
+  }
 });
